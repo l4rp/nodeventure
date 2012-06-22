d37a470c1279013df2db083d15ee71bae606e335
Pete Goodman <thegingerbloke@gmail.com>
2012-06-22 12:20:00 +0100

    adding welcome message

--- client/js/script.js
+++ client/js/script.js
@@ -1,27 +1,29 @@
 var socket = io.connect(location.href);
 
+
+// function to add new text to the page
 function addLine(string) {
   var line = $('<pre>');
   line.text(string);
   $('#output').append(line);
 }
 
+
+// set up sockets
 socket.on('write', function (string) {
   addLine(string);
 });
-
 socket.on('disconnect', function () {
   addLine('DISCONNECTED!');
   addLine('Reload to reconnect...');
 });
 
-socket.emit('login', prompt("Name?"));
 
+// function to send data
 function sendCommand() {
   socket.emit('command', $('#command').val());
   $('#command').val('').focus();
 }
-
 $('#send').click(sendCommand);
 $('#command').keyup(function (e) {
   if (e.keyCode === 13) {
@@ -29,8 +31,40 @@ $('#command').keyup(function (e) {
   }
 });
 
-addLine('Connecting...');
 
+// init the page on load
+function init() {
+  var welcome, $line, counter, length;
+  welcome = '              _                 _       \n _ _  ___  __| |_____ _____ _ _| |_ _  _ _ _ ___ \n| \' \\/ _ \\/ _` / -_) V / -_) \' \\  _| || | \'_/ -_)\n|_||_\\___/\\__,_\\___|\\_/\\___|_||_\\__|\\_,_|_| \\___|';
+  $line = $('<pre>');
+  $('#output').append($line);
 
-// JS finished initing, focus on text area
-$("input#command").focus();
+  counter = 0;
+  length = welcome.length;
+  addChar();
+
+  // add characters, one at a time
+  function addChar() {
+    $line.append(welcome.charAt(counter));
+    
+    // are we still adding chars?
+    if (counter++ < length) {
+
+      // don't delay on spaces
+      if (welcome.charAt(counter) === " ") {
+        addChar();
+      } else {
+        setTimeout(addChar, 50);
+      }
+    
+    // we've finished adding characters, init
+    } else {
+      socket.emit('login', prompt("Name?"));
+      addLine('Connecting...');
+      $("input#command").focus();
+    }
+  }
+}
+
+// INIT !
+init();
