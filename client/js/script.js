68f3b9c0c640584df12aab9a3ba4c87e5a501c39
Mike Allanson <michael.allanson@gmail.com>
2012-06-22 12:46:32 +0100

    added some effects

--- client/js/script.js
+++ client/js/script.js
@@ -32,5 +32,17 @@ $('#command').keyup(function (e) {
 addLine('Connecting...');
 
 
+function blur() {
+  $('body').addClass('blurry');
+}
+
+var colours = ['#ff0000', '#00ff00', '#0000ff', '#cc9943'];
+function wooaah() {
+  var colourA = colours[Math.floor(Math.random() * colours.length)];
+  var colourB = colours[Math.floor(Math.random() * colours.length)];
+  $('pre').animate({color: colourA}, Math.random() * 500);
+  $('body').animate({backgroundColor: colourB}, Math.random() * 800, wooaah);
+}
+
 // JS finished initing, focus on text area
 $("input#command").focus();
