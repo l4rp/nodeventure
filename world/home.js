b7e122d55587997e12c8af67aea298a8ad6cb989
Aaron Acerboni <a.acerboni@gmail.com>
2012-06-22 10:58:29 +0100

    Add 2 rooms

--- world/home.js
+++ world/home.js
@@ -3,5 +3,14 @@ room('home', {
   exits: {west: 'meeting-room'}});
 
 room('meeting-room', {
-  description: "This is the breakout meeting room for the Lab for the Recently Possible",
-  exits: {east: 'home'}});
+  description: "This is the breakout meeting room for the Lab for the Recently Possible. There is a dark hole here.",
+  exits: {east: 'home', down: 'hole'}});
+
+room('hole', {
+  description: "You are in a deep, dark hole.",
+  exits: {up: 'hole2'}});
+
+room('hole2', {
+  description: "You scramble and fail to get out. You are trapped here forever. Perhaps you can cry?",
+  exits: {up: 'hole2'}});
+
