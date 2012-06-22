59a84feded489e79410b8b33f54a502fb372f5b4
Simon Elliott <simon@papercreatures.com>
2012-06-22 12:16:30 +0100

    event on running command

--- game.js
+++ game.js
@@ -53,6 +53,7 @@ _.extend(Game.prototype, {
     } else {
       try {
         this.commands[command](rest, player, this);
+        this.emit('command:'+command, rest, player, this);
       } catch (e) {
         console.log('Error running command: ' + string);
         console.log(e);
